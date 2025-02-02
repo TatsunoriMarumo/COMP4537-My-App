ffef49a8c157a1ef2845944f2be1fe9071c0c145
TatsunoriMarumo <t.maru0910@gmail.com>
2025-02-01 22:05:12 -0800

    Create read file api

--- labs/3/api.js
+++ labs/3/api.js
@@ -1,38 +1,70 @@
 const http = require("http");
 const url = require("url");
 const fs = require("fs");
-const dt = require("../../modules/utils")
-const messageData = require("../../lang/en/en.json")
+const dt = require("../../modules/utils");
+const messageData = require("../../lang/en/en.json");
+const path = require("path");
+
+const getDate = (req, res) => {
+  const { query } = url.parse(req.url, true);
+  const name = query.name || "Guest";
+  const message = messageData.message;
+  const serverTime = dt.myDateTime();
+  const formattedMessage = message.replace("%1", name) + ` ${serverTime}`;
+
+  res.writeHead(200, { "Content-type": "text/html" });
+  res.end(`<p style="color: blue; font-size: 18px;">${formattedMessage}</p>`);
+};
+
+const writeFile = async (req, res) => {
+  const { query } = url.parse(req.url, true);
+  const text = query.name;
+  if (!text) {
+    res.writeHead(400, { "content-type": "text/plain" });
+    res.end("Missing 'name' parameter");
+    return;
+  }
+  const success = await FileHandler.appendFile("file.txt", text);
+  res.writeHead(success ? 200 : 500, { "content-type": "text/plain" });
+  res.end(
+    success ? "Text successfully appended to file." : "Error writing to file."
+  );
+};
 
 
-const getDate = ((req, res) => {
-    const q = url.parse(req.url, true)
-    const name = q.query.name || "Guest"
-    const message = messageData.message;
-    const serverTime = dt.myDateTime();
-    const formattedMessage = message.replace("%1", name) + ` ${serverTime}`
+const readFile = async (req, res) => {
+    const { pathname } = url.parse(req.res, true)
+    const filename = path.basename(pathname)
+    const data = await FileHandler.readFile(filename)
+    res.writeHead(data ? 200 : 404, { "content-type": "text/plain" });
+    res.end(
+        data ? data : "404 File Not Found."
+    )
+}
 
-    res.writeHead(200, {"Content-type": "text/html"})
-    res.end(`<p style="color: blue; font-size: 18px;">${formattedMessage}</p>`)
-})
+class FileHandler {
+  static async appendFile(filename, text) {
+    try {
+      await fs.appendFile(filename, text + "\n");
+      return true;
+    } catch (err) {
+      console.error("Error writing to file:", err);
+      return false;
+    }
+  }
 
-const writeFile = ((req, res) => {
-    const file = "file.txt"
-    const q = url.parse(req.url, true)
-    const text = q.query.name
-    fs.appendFile(file, text + "\n", (err) => {
-        if (err) {
-            res.writeHead(500, { "Content-type": "text/plain" });
-            res.end("Error occurs while writing to file.")
-            return
-        }
-    }) 
-    res.writeHead(200, { "Content-type": "text/plain" })
-    res.end("Text successfully appended to file.")
-})
+  static async readFile(filename) {
+    try {
+      return await fs.readFile(filename, "utf8");
+    } catch (err) {
+      console.log("Error reading file:", err);
+      return null;
+    }
+  }
+}
 
-module.exports = { 
-    getDate,
-    writeFile,
- };
-
+module.exports = {
+  getDate,
+  writeFile,
+  readFile,
+};
--- server.js
+++ server.js
@@ -1,20 +1,27 @@
 const http = require("http");
 const url = require("url");
-const { getDate, writeFile } = require("./labs/3/api")
+const path = require("url");
+const { getDate, writeFile, readFile } = require("./labs/3/api");
 
-const PORT = process.env.PORT || 8888
+const PORT = process.env.PORT || 8888;
 
-http.createServer((req, res) => {
-    const q = url.parse(req.url, true)
-    const pathname = q.pathname
+http
+  .createServer((req, res) => {
+    const { pathname } = url.parse(req.url, true);
 
-    if (pathname === "/COMP4537/labs/3/getDate") {
-        getDate(req, res)
-        return
+    switch (true) {
+      case pathname.startsWith("/COMP4537/labs/3/getDate"):
+        return getDate(req, res);
+
+      case pathname.startsWith("/COMP4537/labs/3/writeFile"):
+        return writeFile(req, res);
+
+      case pathname.startsWith("/COMP/4537/labs/3/readText"):
+        return readFile(req, res);
+
+      default:
+        res.writeHead(404, { "content-type": "text/plain" });
+        res.end("404 Not Found");
     }
-
-    if (pathname === "/COMP4537/labs/3/writeFile") {
-        writeFile(req, res)
-        return
-    }
-}).listen(PORT)
+  })
+  .listen(PORT);
