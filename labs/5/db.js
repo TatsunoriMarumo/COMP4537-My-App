49bf9eca93a7a3fb4d8c9b1113d19d9875df5f66
TatsunoriMarumo <t.maru0910@gmail.com>
2025-02-16 01:18:21 -0800

    update mysql

--- labs/5/db.js
+++ labs/5/db.js
@@ -1,4 +1,4 @@
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const path = require('path');
 const fs = require("fs");
 require('dotenv').config({ path: path.resolve(__dirname, '.env') });
